7dc165f6dfeb1f6cc3702d82c28a60f588a3f964
Utkarsh <tu858371@gmail.com>
2023-06-22 20:21:54 +0530

    Synchronous timer v1

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+//Getting http module
+const http = require("http");
+//forming http server and passing express app as request handler
+const server = http.createServer(app);
+const path = require("path");
+const socketio = require("socket.io");
+const io = socketio(server);
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+
+let timer;
+
+io.on("connection", (socket) => {
+
+    // console.log(`Someone got connected with the id - ${socket.id}`);
+
+
+    //when user sends request with event type: countdown-set
+    socket.on("countdown-set", (data) => {
+      // console.log(`${data.hrs}:${data.mins}:${data.sec}`);
+      timer = parseInt(data.hrs) * 3600 + parseInt(data.mins) * 60 + parseInt(data.sec);
+      // console.log(`${timer} seconds`);
+
+      // console.log(timer);
+
+      startCountdown();
+
+      function startCountdown() {
+
+          const id = setInterval(countdown, 1000);
+          function countdown() {
+
+              //If timer up
+              if (timer === -1) {
+                clearInterval(id);
+              } 
+              else{
+                
+                //emit countdown status to all users currently connected to server
+                io.emit("current-timer", {
+                  currentTimer: timer
+                })
+                
+                //reduce timer for each second passing
+                timer = timer - 1;
+              }
+          }
+      }  
+    
+  });
+
+  //when user sends request (for stopping timer) of event type: stop-timer
+  //Then setting timer to zero(0)
+  socket.on("stop-timer", (data) => {
+    timer = 0;
+  })
+  
+});
+
+
+const port = 3000;
+
+//method provided by node js http module
+server.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
+
---
+++ public/script.js
@@ -0,0 +1,43 @@
+const socket = io();
+
+$("#set-timer").click(() => {
+    const hrs = $("#hours").val();
+    const mins = $("#minutes").val();
+    const sec = $("#seconds").val();
+
+    socket.emit("countdown-set", {
+        hrs:hrs,
+        mins:mins,
+        sec:sec
+    })
+});
+
+socket.on("current-timer", (data) => {
+
+    let currentTimer = data.currentTimer;
+
+    let hh = 0;
+    let mm = 0;
+    let ss = 0;
+
+    hh = Math.floor(currentTimer / 3600);
+    currentTimer = currentTimer - (3600 * hh);
+
+    mm = Math.floor(currentTimer / 60);
+    currentTimer = currentTimer - (60 * mm);
+
+    ss = currentTimer;
+
+    $("#timer-hrs").html(`<b>${hh}:</b>`);
+    $("#timer-mins").html(`<b>${mm}:</b>`);
+    $("#timer-sec").html(`<b>${ss}</b>`)
+})
+
+$("#reset-timer").click(() => {
+    socket.emit("stop-timer",{});
+})
+
+
+
+
+
